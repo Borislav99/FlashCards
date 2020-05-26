e605f3df5829038bafa34c291127b39b5431066c
Borislav Adamovic <borislavadamovic9@gmail.com>
2020-05-26 18:44:40 +0200

    added validator for each field

--- create/create.js
+++ create/create.js
@@ -1,70 +1,3 @@
-/* ------------------------------------------------------------ BASIC JS ------------------------------------------------------------ */
-(function () {
-  /* ------------------------------ KLASE ------------------------------ */
-  /* ---------- KLASA UI ---------- */
-  class UI {
-    //konstruktor
-    constructor() {
-      this.icon = appMenu.children[0].children[0];
-      this.menu = document.querySelector(".menu");
-    }
-    //dobijanje vrijednosti na osnovu koje se radi funkcija hideShowMenu
-    getValue() {
-      let value;
-      if (this.icon.classList.contains("down")) {
-        value = "down";
-      } else if (this.icon.classList.contains("up")) {
-        value = "up";
-      }
-      return value;
-    }
-    //prikazi i sakrij meni na osnovu vrijednosti koje si dobio
-    hideShowMenu(value) {
-      if (value === "down") {
-        //akcije sa ikonom
-        this.icon.classList.remove("down", "fas", "fa-angle-down");
-        this.icon.classList.add("up", "fas", "fa-angle-up");
-        //akcije sa menijem
-        this.menu.classList.remove("hidemenu");
-        this.menu.classList.add("showmenu");
-      } else if (value === "up") {
-        //akcije sa ikonom
-        this.icon.classList.remove("up", "fas", "fa-angle-up");
-        this.icon.classList.add("down", "fas", "fa-angle-down");
-        //akcije sa menijem
-        this.menu.classList.add("hidemenu");
-        this.menu.classList.remove("showmenu");
-      }
-    }
-  }
-  /* ---------- KLASA UI ---------- */
-
-  /* ------------------------------ KLASE ------------------------------ */
-
-  /* ------------------------------ VARIJABLE ------------------------------ */
-
-  let appMenu = document.querySelector(".appMenu");
-  let ui = new UI();
-
-  /* ------------------------------ VARIJABLE ------------------------------ */
-
-  /* ------------------------------ EVENTI ------------------------------ */
-
-  //prikazi i sakrij meni
-  appMenu.addEventListener("click", (event) => {
-    if (event.target.parentElement.classList.contains("openMenu")) {
-      let value = ui.getValue();
-      ui.hideShowMenu(value);
-    } else if (event.target.classList.contains("appMenu")) {
-      let value = ui.getValue();
-      ui.hideShowMenu(value);
-    }
-  });
-
-  /* ------------------------------ EVENTI ------------------------------ */
-})();
-/* ------------------------------------------------------------ BASIC JS ------------------------------------------------------------*/
-
 /* ------------------------------------------------------------ FORMATTING JS ------------------------------------------------------------*/
 (function () {
   /* ------------------------------ KLASE ------------------------------ */
@@ -241,7 +174,19 @@
       return filteredValidator;
     }
     validateFields(validator){
-      
+      if (validator.includes('Please enter value for Stack Name')){
+        this.pleaseValidate(stackInput)
+      } if (validator.includes('Please enter value for Task')) {
+        this.pleaseValidate(taskInput)
+      } if (validator.includes('Please enter value for Solution')) {
+        this.pleaseValidate(solutionInput)
+      } 
+    }
+    pleaseValidate(field){
+      field.classList.add('validateThis');
+      setTimeout(()=>{
+        field.classList.remove('validateThis');
+      },3000)
     }
     clearFields() {
       stackInput.value = "";
